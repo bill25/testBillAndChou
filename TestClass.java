1bf6bb470175645bbfd7c53a93ee1d5249678866
BChouaib <bc.chouaib@gmail.com>
2016-06-01 11:22:05 +0100

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 	public static void main(String[] args) {
 	  System.out.println("lgodam brk a lbill");
 	  System.out.println("ani dertha wzedte modifite fiha");
+	  System.out.println("ani ga3ed n commiti ghir fel master branch chouf les historiques");
 	}
 }	
